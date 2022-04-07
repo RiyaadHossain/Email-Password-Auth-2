98036d333de90d9943a075afdae6d90b87e535c7
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-07 17:53:04 +0600

    Basic form displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import "./App.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <h1 className="text-center mt-5 mb-4 text-primary">Submit the Form</h1>
+      <div className="border py-3 px-3 shadow rounded-3 mt-3">
+        <h2 className="text-center mb-3 text-info">Sign Up</h2>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
